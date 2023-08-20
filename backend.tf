3b02bafaed26e6edd75be7865a646b571b9a14cb
9766729225 <amolbhor9766@gmail.com>
2023-08-20 18:39:09 +0530

    first

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-project-1-terraform"
-    key = "main"
+    key = "amol"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
